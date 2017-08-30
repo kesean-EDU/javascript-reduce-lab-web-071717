0b8c88c2d09e092d7d777786b8401086d3b01a26
kewoodh <kesean.woodhouse@gmail.com>
2017-08-30 17:28:29 -0400

    Done.

--- index.js
+++ index.js
@@ -16,3 +16,19 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+let totalBatteries = batteryBatches.reduce(function(total,batch){
+  return total+=batch;
+},0);
+
+let wordCountMap = monologueLines.reduce((map, sentence) => {
+  let wordCount = sentence.split(' ').length;
+
+  if (!map[wordCount]) {
+    map[wordCount] = 0;
+  }
+
+  map[wordCount]++;
+
+  return map;
+}, {});
